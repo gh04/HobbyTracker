b50cb2f7bf419c613376013bd76fadf2ba90290c
Gerardo Hernandez <gahernandez02@gmail.com>
2019-11-15 19:23:37 -0800

    Push with completed views

---
+++ HobbyTracker/FriendsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FriendsTableViewController.swift
+//  HobbyTracker(iOSPT4)
+//
+//  Created by Johnny Hicks on 11/14/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
